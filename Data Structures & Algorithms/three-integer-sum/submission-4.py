a14bcfd197077d12a6f11f519cbb6cdaddb9a7db
spxdot19 <71119479+spxdot19@users.noreply.github.com>
2026-04-07 00:44:26 +1000

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        # print(nums)
+        stack = [] 
+        n = len(nums)
+        for i in range(n):
+            l = i+1
+            r = n-1
+            while l<r :
+                aim = -nums[i]
+                subStack = [nums[i],nums[l],nums[r]]
+                if sum(subStack)==0 and subStack not in stack:
+                    stack.append(subStack)
+                    l+=1
+                    r-=1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+                else:
+                    if aim > nums[l] + nums[r]:
+                        l+=1
+                    else:
+                        r-=1
+        return stack
+                    
+
+
+
